fefb78ad371ce2ac8a969395dd87ccddf782d053
Hajime Kosaku <kosakku0105@gmail.com>
2015-11-17 13:51:14 +0900

    delete CheckRun.r

--- CheckRun.r
+++
@@ -1,8 +0,0 @@
-library(ltm)
-
-source("changeData.r")
-data <- read.csv("dataLogic.csv", header = FALSE)
-mod <- rasch(data, IRT.param = TRUE)
-print(mod)
-plot(mod, lwd = 3, cex.axis = 2, cex.lab = 1.5, xlab = "θ", ylab = "P(θ)", main = "")
-print(summary(mod))
